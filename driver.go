95cf3ae54eed48ab1fee1e5425d192a0e34cb6c8
cristian ortiz navia <cristian.ortiznavia82@gmail.com>
2022-02-23 23:53:28 -0300

    fixing "panic: sql: Register called twice for driver pgx"
    the problem is the incorrect order in jackc/* imports, must be
    pgconn
    pgx/v4
    pgx.v4/stdlib

--- driver.go
+++ driver.go
@@ -4,8 +4,8 @@ import (
 	"database/sql"
 
 	_ "github.com/jackc/pgconn"
-	_ "github.com/jackc/pgx/stdlib"
 	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 //OpenDB open a DB connection with postgres redis or mariaDB engine
